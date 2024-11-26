2c744f5c56d4e9957a20eec5860527178d1aa2e2
Khushil-21 <khushilshah2105@gmail.com>
2024-11-26 15:04:30 +0530

    new

--- server/app.py
+++ server/app.py
@@ -374,12 +374,12 @@ async def insert_data(data: List[TrademarkData]):
     response_description="Welcome message",
 )
 async def getApi():
-    return {message:"Hello From Backend"}
+    return {"message": "Hello From Backend"}
 
 if __name__ == "__main__":
     import uvicorn
 
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
     # Run server using: python app.py
     # Or using uvicorn directly: uvicorn app:app --host 0.0.0.0 --port 8000 --reload
