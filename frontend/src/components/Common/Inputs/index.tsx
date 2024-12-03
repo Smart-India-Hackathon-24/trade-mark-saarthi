1a45c16d27a88bd603db33c17a093fd6fea22e4a
Vinit-180 <vinitchokshi1809@gmail.com>
2024-12-03 21:55:49 +0530

    Similarity API Integration and Table Component

--- frontend/src/components/Common/Inputs/index.tsx
+++ frontend/src/components/Common/Inputs/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-interface InputFields{
+type InputFields={
     label:string;
     name:string;
     type:string;
+    step?:string;
     placeholder?:string;
     required?: boolean;
     disabled?:boolean;
 }
 
 
-const InputField:React.FC<InputFields>=({label,name,type,placeholder,required,disabled=false})=>{
+const InputField:React.FC<InputFields>=({label,name,type,placeholder,required,disabled=false,step})=>{
     return (<div className="py-2">
         <label htmlFor={name} className="block text-sm font-medium  mb-1">
         {label}
@@ -22,6 +23,7 @@ const InputField:React.FC<InputFields>=({label,name,type,placeholder,required,di
         placeholder={placeholder}
         required={required}
         disabled={disabled}
+        step={step}
         className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-black"
       />
     </div>)
